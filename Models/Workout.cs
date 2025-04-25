e355db5c23d39cc57391ef489817fdf74397ad69
Vladislav Petković <121941612+SaladVlad@users.noreply.github.com>
2025-04-25 21:12:24 +0200

    Changing model inaccuracy and migration to the db

--- Models/Workout.cs
+++ Models/Workout.cs
@@ -7,11 +7,10 @@
         public WorkoutType Type { get; set; }
         public int DurationMinutes { get; set; }
         public int CaloriesBurned { get; set; }
-        public int Intensity { get; set; } // 1–10
-        public int Fatigue { get; set; } // 1–10
+        public int Intensity { get; set; }
+        public int Fatigue { get; set; } 
         public string? Notes { get; set; } = null;
         public DateTime PerformedAt { get; set; }
-        public required User User { get; set; }
     }
 
     public enum WorkoutType

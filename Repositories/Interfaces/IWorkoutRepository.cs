101b19ab8c2562cac00833870400b4d28453a1ef
Vladislav Petković <121941612+SaladVlad@users.noreply.github.com>
2025-04-25 21:14:22 +0200

    Adding methods to interfaces for easier centralization

--- Repositories/Interfaces/IWorkoutRepository.cs
+++ Repositories/Interfaces/IWorkoutRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace GymTrackerAPI.Repositories.Interfaces
+﻿using GymTrackerAPI.DTOs.Workouts;
+using GymTrackerAPI.Models;
+
+namespace GymTrackerAPI.Repositories.Interfaces
 {
     public interface IWorkoutRepository
     {
+        Task CreateWorkoutAsync(Workout workout);
+        Task<UserWorkoutsResponseDto?> GetAllUserWorkoutsAsync(Guid userId);
+        Task<UserWorkoutsResponseDto?> GetWorkoutsByDateRangeAsync(Guid userId, DateTime startDate, DateTime endDate);
     }
 }
--- Services/Interfaces/IWorkoutService.cs
+++ Services/Interfaces/IWorkoutService.cs
@@ -1,6 +1,13 @@
-﻿namespace GymTrackerAPI.Services.Interfaces
+﻿using GymTrackerAPI.DTOs.Workouts;
+
+namespace GymTrackerAPI.Services.Interfaces
 {
     public interface IWorkoutService
     {
+        Task<CreateWorkoutResponseDto?> CreateWorkoutAsync(Guid userId, CreateWorkoutDto dto);
+        Task<UserWorkoutsResponseDto?> GetUserWorkoutsAsync(Guid userId);
+
+        List<(DateTime Start, DateTime End)> GetLogicalWeeks(int year, int month);
+        Task<UserWorkoutsResponseDto?> GetWorkoutsByDateRangeAsync(Guid userId, DateTime startDate, DateTime endDate);
     }
 }

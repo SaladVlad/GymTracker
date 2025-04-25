6760b91bd0ae3586068152e164923e7f56d27e3d
Vladislav Petković <121941612+SaladVlad@users.noreply.github.com>
2025-04-25 21:15:22 +0200

    Helper function to calculate date ranges

---
+++ Utils/DateHelper.cs
@@ -0,0 +1,29 @@
+﻿namespace GymTrackerAPI.Utils
+{
+    public static class DateHelper
+    {
+        public static List<(DateTime Start, DateTime End)> GetLogicalWeeksInMonth(int year, int month)
+        {
+            var result = new List<(DateTime Start, DateTime End)>();
+
+            var firstDay = new DateTime(year, month, 1);
+            var lastDay = firstDay.AddMonths(1).AddDays(-1);
+
+            var currentStart = firstDay;
+
+            while (currentStart <= lastDay)
+            {
+                var currentEnd = currentStart.AddDays(6);
+                if (currentEnd > lastDay)
+                    currentEnd = lastDay;
+
+                result.Add((currentStart, currentEnd));
+                currentStart = currentEnd.AddDays(1);
+            }
+
+            return result;
+        }
+
+
+    }
+}
